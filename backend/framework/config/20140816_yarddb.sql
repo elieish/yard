3222a2ab362e45f067cf6a27e3f500f3100d5564
Elie <eli@implyit.co.za>
2014-08-16 12:55:26 +0200

    Statement

--- backend/framework/config/20140816_yarddb.sql
+++ backend/framework/config/20140816_yarddb.sql
@@ -17,10 +17,11 @@ USE `yarddb`;
 
 -- Dumping structure for table yarddb.enterprises
 CREATE TABLE IF NOT EXISTS `enterprises` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `uid` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `group_id` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`uid`
+)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table yarddb.enterprises: ~0 rows (approximately)
@@ -30,14 +31,15 @@ CREATE TABLE IF NOT EXISTS `enterprises` (
 
 -- Dumping structure for table yarddb.enterprise_groups
 CREATE TABLE IF NOT EXISTS `enterprise_groups` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `uid` int(10) NOT NULL AUTO_INCREMENT,
   `group` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`uid`
+)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table yarddb.enterprise_groups: ~0 rows (approximately)
 /*!40000 ALTER TABLE `enterprise_groups` DISABLE KEYS */;
-INSERT INTO `enterprise_groups` (`id`, `group`) VALUES
+INSERT INTO `enterprise_groups` (`uid`, `group`) VALUES
 	(1, 'SMME'),
 	(2, 'Co-op'),
 	(3, 'Individual');
@@ -46,7 +48,7 @@ INSERT INTO `enterprise_groups` (`id`, `group`) VALUES
 
 -- Dumping structure for table yarddb.members
 CREATE TABLE IF NOT EXISTS `members` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `uid` int(10) NOT NULL AUTO_INCREMENT,
   `membership_no` varchar(50) DEFAULT NULL,
   `title_id` int(11) NOT NULL DEFAULT '0',
   `name` varchar(50) NOT NULL,
@@ -64,7 +66,8 @@ CREATE TABLE IF NOT EXISTS `members` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`uid`
+)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table yarddb.members: ~0 rows (approximately)
@@ -74,14 +77,15 @@ CREATE TABLE IF NOT EXISTS `members` (
 
 -- Dumping structure for table yarddb.provincies
 CREATE TABLE IF NOT EXISTS `provincies` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `uid` int(10) NOT NULL AUTO_INCREMENT,
   `province` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`uid`
+)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table yarddb.provincies: ~0 rows (approximately)
 /*!40000 ALTER TABLE `provincies` DISABLE KEYS */;
-INSERT INTO `provincies` (`id`, `province`) VALUES
+INSERT INTO `provincies` (`uid`, `province`) VALUES
 	(1, 'Limpopo'),
 	(2, 'Gauteng'),
 	(3, 'North West'),
@@ -96,14 +100,15 @@ INSERT INTO `provincies` (`id`, `province`) VALUES
 
 -- Dumping structure for table yarddb.sectors
 CREATE TABLE IF NOT EXISTS `sectors` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `uid` int(10) NOT NULL AUTO_INCREMENT,
   `sector` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`uid`
+)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table yarddb.sectors: ~0 rows (approximately)
 /*!40000 ALTER TABLE `sectors` DISABLE KEYS */;
-INSERT INTO `sectors` (`id`, `sector`) VALUES
+INSERT INTO `sectors` (`uid`, `sector`) VALUES
 	(1, 'National'),
 	(2, 'Provincial'),
 	(3, 'District'),
@@ -113,14 +118,15 @@ INSERT INTO `sectors` (`id`, `sector`) VALUES
 
 -- Dumping structure for table yarddb.titles
 CREATE TABLE IF NOT EXISTS `titles` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `uid` int(10) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`uid`
+)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table yarddb.titles: ~0 rows (approximately)
 /*!40000 ALTER TABLE `titles` DISABLE KEYS */;
-INSERT INTO `titles` (`id`, `title`) VALUES
+INSERT INTO `titles` (`uid`, `title`) VALUES
 	(1, 'National Chair'),
 	(2, 'National Treasurer'),
 	(3, 'National Secretary'),
@@ -138,7 +144,7 @@ INSERT INTO `titles` (`id`, `title`) VALUES
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 
 CREATE TABLE `users` (
-  `id`       int(11)     auto_increment,
+  `uid`       int(11)     auto_increment,
   `datetime`      datetime    NOT NULL default '0000-00-00 00:00:00',
   `user`        int(11)     NOT NULL default 0,
   `username`      varchar(50)   NOT NULL default '',
@@ -151,26 +157,29 @@ CREATE TABLE `users` (
   `fax`       varchar(30)   NOT NULL default '',
   `notes`       varchar(255)  NOT NULL default '',
   `active`      int(1)      NOT NULL default 1,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`uid`
+)
 );
 
 CREATE TABLE `groups` (
-  `id`       int(11)     auto_increment,
+  `uid`       int(11)     auto_increment,
   `datetime`      datetime    NOT NULL default '0000-00-00 00:00:00',
   `user`        int(11)     NOT NULL default 0,
   `name`        varchar(100)  NOT NULL default '',
   `active`      int(1)      NOT NULL default 1,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`uid`
+)
 );
 
 CREATE TABLE `user_groups` (
-  `id`       int(11)     auto_increment,
+  `uid`       int(11)     auto_increment,
   `datetime`      datetime    NOT NULL default '0000-00-00 00:00:00',
   `user`        int(11)     NOT NULL default 0,
   `user_id`     int(11)     NOT NULL default 0,
   `group_id`      int(11)     NOT NULL default 0,
   `active`      int(1)      NOT NULL default 1,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`uid`
+)
 );
 
 INSERT INTO `users` (`datetime`, `username`, `password`, `first_name`, `last_name`, `active`) VALUES(NOW(), 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Admin', 'User', 1);
