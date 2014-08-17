89fba4aa941fc1fb0e855e233b90eea1f435b4bf
Elie <eli@implyit.co.za>
2014-08-17 12:26:14 +0200

    Changes config log

--- backend/framework/config/config.php
+++ backend/framework/config/config.php
@@ -12,21 +12,21 @@
 # =========================================================================
 
 # System Settings
-$_GLOBALS['title']														= "AME Email2Fax Service";
-$_GLOBALS['max_results']												= 20;
-$_GLOBALS['default_page']												= "home";
-$_GLOBALS['default_action']												= "display";
-$_GLOBALS['log_file']													= "/var/log/ame_fax/" . date("Y-m-d") . ".log";
-$_GLOBALS['base_dir']													= dirname(dirname(dirname(dirname(__FILE__)))) . "/";
-$_GLOBALS['base_url']													= $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']) . "/../../";
-$_GLOBALS['upload_dir']													= $_GLOBALS['base_dir'] . "files/";
-$_GLOBALS['upload_url']													= "files/";
-$_GLOBALS['admin_email']												= "development@Yard.co.za";
-$_GLOBALS['security_email']												= "development@Yard.co.za";
-$_GLOBALS['security_subject']											= "SECURITY ALERT FOR " . $_GLOBALS['title'];
-$_GLOBALS['from_email']													= "no-reply@Yard.co.za";
-$_GLOBALS['config_file']												= dirname(__FILE__) . "/config.ini";
-$_GLOBALS['requires_login']												= true;
+$_GLOBALS['title']											= "Yard";
+$_GLOBALS['max_results']									= 20;
+$_GLOBALS['default_page']									= "home";
+$_GLOBALS['default_action']									= "display";
+$_GLOBALS['log_file']										= "/var/log/yard/" . date("Y-m-d") . ".log";
+$_GLOBALS['base_dir']										= dirname(dirname(dirname(dirname(__FILE__)))) . "/";
+$_GLOBALS['base_url']									    = $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']) . "/../../";
+$_GLOBALS['upload_dir']										= $_GLOBALS['base_dir'] . "files/";
+$_GLOBALS['upload_url']										= "files/";
+$_GLOBALS['admin_email']									= "development@yard.co.za";
+$_GLOBALS['security_email']									= "development@yard.co.za";
+$_GLOBALS['security_subject']								= "SECURITY ALERT FOR " . $_GLOBALS['title'];
+$_GLOBALS['from_email']										= "no-reply@tard.co.za";
+$_GLOBALS['config_file']									= dirname(__FILE__) . "/config.ini";
+$_GLOBALS['requires_login']									= true;
 
 # =========================================================================
 # THE END
