6c74c1c4a0799147a09b2ffe75debe4d17580029
Bongekile <tenzabonge@gmail.com>
2014-08-24 13:48:12 +0200

    Fixes css problems

--- js/main.js
+++ js/main.js
@@ -1,28 +1,35 @@
-$(document).ready(function(){
+jQuery(document).ready(function($) {
 
-	$( "#registration" ).submit( function(event) {
-		var name          = $("#name").val().toString().substr(0,1).toUpperCase();
-		var surname       = $("#surname").val().toString().substr(0,1).toUpperCase();
-		var fullDate      = new Date();console.log(fullDate);
-		var twoDigitMonth = fullDate.getMonth()+"";if(twoDigitMonth.length==1)  twoDigitMonth="0" +twoDigitMonth;
-		var twoDigitDate  = fullDate.getDate()+"";if(twoDigitDate.length==1) twoDigitDate="0" +twoDigitDate;
-		var currentDate   = twoDigitDate  + twoDigitMonth + fullDate.getFullYear().toString().substr(2,2);
-		var province	  = $("#province").val();
-		var membershipno = name + surname + currentDate + province;
-		var text = "Thank you for your registration,your membership number is "+ membershipno;
-		$("#registrationnumber").val(membershipno);
-		alert(text);
-		//$("#confirmationmembership").innerHTML="text";
-		//$("#ishimwe").modal('show');
-		//event.preventDefault();
-	});
+	'use strict';
+
+		$(".our-listing").owlCarousel({
+			items: 4,
+			navigation: true,
+			navigationText: ["&larr;","&rarr;"],
+		});
 
 
+		$('.flexslider').flexslider({
+		    animation: "fade",
+		    controlNav: false,
+		    prevText: "&larr;",
+		    nextText: "&rarr;"
+		});
 
-	  // Datepicker for single fields
-    $('.date').datepicker({
-        dateFormat: 'yy-mm-dd'
-    });
+
+		$('.toggle-menu').click(function(){
+	        $('.menu-responsive').slideToggle();
+	        return false;
+	    });
+
 
 });
+
+
+
+
+
+
+
+
 
