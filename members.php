9a50b62d1d8ab8a06494f22ee746955801ee1103
Kuthula Langa <langa@implyit.co.za>
2014-10-07 20:23:43 +0200

    Added stats on members page

--- members.php
+++ members.php
@@ -18,6 +18,26 @@ Application::db_connect();
 $title          = titles_select();
 $province       = provinces_select();
 $date_select    = html_date();
+
+#Global Variables
+global $_db;
+
+$no_of_members		= $_db->fetch_single("SELECT COUNT('uid') AS 'count' FROM members");
+$no_of_females		= $_db->fetch_single("SELECT COUNT('uid') AS 'count' FROM members WHERE gender=2");
+$no_of_males		= $_db->fetch_single("SELECT COUNT('uid') AS 'count' FROM members WHERE gender=1");
+
+$members_15_20		= $_db->fetch_single("SELECT COUNT('uid') AS 'count' FROM members WHERE (YEAR(now()) - YEAR(dob)) >= 15 AND (YEAR(now()) - YEAR(dob)) <= 20");
+$members_21_25		= $_db->fetch_single("SELECT COUNT('uid') AS 'count' FROM members WHERE (YEAR(now()) - YEAR(dob)) >= 21 AND (YEAR(now()) - YEAR(dob)) <= 25");
+$members_26_30		= $_db->fetch_single("SELECT COUNT('uid') AS 'count' FROM members WHERE (YEAR(now()) - YEAR(dob)) >= 26 AND (YEAR(now()) - YEAR(dob)) <= 30");
+
+$members_31_35		= $_db->fetch_single("SELECT COUNT('uid') AS 'count' FROM members WHERE (YEAR(now()) - YEAR(dob)) >= 31 AND (YEAR(now()) - YEAR(dob)) <= 35");
+$no_of_districts	= $_db->fetch_single("SELECT COUNT('uid') AS 'count' FROM districts");
+$no_of_locals		= $_db->fetch_single("SELECT COUNT('uid') AS 'count' FROM locals");
+
+$no_of_coops		= $_db->fetch_single("SELECT COUNT('uid') AS 'count' FROM enterprises WHERE group_id=2");
+$no_of_smmes		= $_db->fetch_single("SELECT COUNT('uid') AS 'count' FROM enterprises WHERE group_id=1");
+$no_of_individuals	= $_db->fetch_single("SELECT COUNT('uid') AS 'count' FROM enterprises WHERE group_id=3");
+
 ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
@@ -123,15 +143,46 @@ $date_select    = html_date();
                 <div class ="row">
                   <div class="col-md-12">
                   <div class="sample-thumb">
-                        <div>
+                  	 <div class="panel panel-default">
                             <ul class="nav nav-pills nav-stacked">
                               <li class="active">
-                              <a>Members </a></li>
+                              <a><span class="glyphicon glyphicon-stats"></span> Statistics</a></li>
                             </ul>
-                            </div>
-
-
-                            <img src="images/member10.jpg" alt="New Event" title="New Event"/>
+                  		<div  class="panel-body">
+                        	
+							<div class="row">
+								<div class="col-lg-3">
+									<ul class="list-group">
+									  <li class="list-group-item list-group-item-success btn-xs"><span class="badge"><?php echo $no_of_members;?></span>No. of Members</li>
+									  <li class="list-group-item list-group-item-info btn-xs"><span class="badge"><?php echo $no_of_females;?></span>No. of Females</li>
+									  <li class="list-group-item list-group-item-warning btn-xs"><span class="badge"><?php echo $no_of_males;?></span>No. of Males</li>
+									 </ul>
+								</div>
+								<div class="col-lg-3">
+									<ul class="list-group">
+									  <li class="list-group-item list-group-item-success btn-xs"><span class="badge"><?php echo $members_15_20;?></span>Members aged 15-20</li>
+									  <li class="list-group-item list-group-item-info btn-xs"><span class="badge"><?php echo $members_21_25;?></span>Members aged 21-25</li>
+									  <li class="list-group-item list-group-item-warning btn-xs"><span class="badge"><?php echo $members_26_30;?></span>Members aged 26-30</li>
+									 </ul>
+								</div>
+								<div class="col-lg-3">
+									<ul class="list-group">
+									  <li class="list-group-item list-group-item-success btn-xs"><span class="badge"><?php echo $members_31_35;?></span>Members aged 31-35</li>
+									  <li class="list-group-item list-group-item-info btn-xs"><span class="badge"><?php echo $no_of_districts;?></span><small>No. of Districts</small></li>
+									  <li class="list-group-item list-group-item-warning btn-xs"><span class="badge"><?php echo $no_of_locals;?></span><small>No. of Local Area/Wards</small></li>
+									 </ul>
+								</div>
+								<div class="col-lg-3">
+									<ul class="list-group">
+									  <li class="list-group-item list-group-item-success btn-xs"><span class="badge"><?php echo $no_of_coops;?></span>No. of Co-ops</li>
+									  <li class="list-group-item list-group-item-info btn-xs"><span class="badge"><?php echo $no_of_smmes;?></span>No. of SMMEs</li>
+									  <li class="list-group-item list-group-item-warning btn-xs"><span class="badge"><?php echo $no_of_individuals;?></span>No. of Individuals</li>
+									 </ul>
+								</div>
+							 </div>
+							</div>
+						   </div>
+                            <!--<img src="images/member10.jpg" alt="New Event" title="New Event"/>-->
 
                             <a href="#" class="list-group-item active"><font size="5"><center>
                                 </center></font>
