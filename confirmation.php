546ec0c872129550b69e5109be3407bb11dc8d1f
Elie Ishimwe <elieish@gmail.com>
2014-10-12 14:06:19 +0200

    Registration Updates

--- confirmation.php
+++ confirmation.php
@@ -63,7 +63,7 @@ $member		= new Member($uid);
           <div class="modal-content">
             <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-              <h4 class="modal-title" id="myModalLabel">YARDMEMBERSHIP CONFIRMATION</h4>
+              <h4 class="modal-title" id="myModalLabel">YARD MEMBERSHIP CONFIRMATION</h4>
             </div>
             <div class="modal-body">
               <div id="text">Thank you for your registration,your membership number is <?php print $member->membership_no ?> </div>
