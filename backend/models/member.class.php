97d77ee32b8381df0e34d09cb4e9d7816785facf
Elie Ishimwe <elieish@gmail.com>
2014-10-12 14:34:32 +0200

    Resolves ##101

--- backend/models/member.class.php
+++ backend/models/member.class.php
@@ -51,18 +51,14 @@ class Member extends Model {
 		$form->add("Name"						, "text"			, "name"				, $this->name);
 		$form->add("Surname"					, "text"			, "surname"				, $this->surname);
 		$form->add("Gender"						, "text"			, "gender"				, $this->gender);
-		$form->add("D.O.B"						, "date"			, "dob"					, $this->dob);
-		$form->add("Age"						, "text"			, "age"					, $this->age);
+		$form->add("Date of Birth"				, "date"			, "dob"					, $this->dob);
 		$form->add_select("Select a Group"   	, "group_id"     	,$this->group_id     	, enterprise_group_select());
 		$form->add("Tel"						, "text"			, "tel"					, $this->tel);
 		$form->add("Cell"						, "text"			, "cell"				, $this->cell);
 		$form->add("Email"						, "text"			, "email"				, $this->email);
 		$form->add_select("Select a Province"   , "province_id"     ,$this->province_id     , province_select()							,array('id', 'province'));
 		$form->add_select("Select a District"   , "district"     	,$this->district     	, districtt_select($this->province_id)		,array('id', 'districts'));
-		//$form->add("District"					, "text"			, "district"			, $this->district);
 		$form->add_select("Select a Local Area" , "local_area"      ,$this->local_area      , local_area_select($this->district)		,array('id', 'locals'));
-		//$form->add("Local Area Name"			, "text"			, "local_area"			, $this->local_area);
-		$form->add_select("Select a Sector"     , "sector_id"       ,$this->sector_id       , sector_select());
 		$form->add(""							, "submit"			, ""					, "Save");
 
 		# Generate HTML
